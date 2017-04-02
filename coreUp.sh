78aa26397b87778039999cf8c78798fc60ac1d19
david@david-mbp <david@davidjenni.com>
2017-04-01 23:55:27 -0700

    ssh port as parameter

--- coreUp.sh
+++ coreUp.sh
@@ -2,6 +2,9 @@
 DO_REGION=${DO_REGION:="sfo1"}
 DO_SIZE=${DO_SIZE:="512mb"}
 DO_NODE_BASENAME=${DO_NODE_BASENAME:="minion"}
+# see channels: https://coreos.com/releases/
+COREOS_REL_CHANNEL=${COREOS_REL_CHANNEL:="alpha"}
+SSH_PORT=${SSH_PORT:="4410"}
 
 hash docker-machine 2>/dev/null || { echo >&2 "Cannot find 'docker-machine', install with 'brew install docker-machine' or visit https://docs.docker.com/machine/install-machine/"; exit 1; }
 [ -n ${DIGITALOCEAN_ACCESS_TOKEN} ] || { echo "Must define env var DIGITALOCEAN_ACCESS_TOKEN with DO app key, see  https://cloud.digitalocean.com/settings/api/tokens" && exit 1; }
@@ -20,20 +23,19 @@ fi
 _CLOUD_CONFIG=${CLOUD_CONFIG:="cloud-config.yaml"}
 _CLOUD_CONFIG_GEN=$(basename -s .yaml ${_CLOUD_CONFIG}).gen.yaml
 
-# sed \
-#     -e "s|{{DO_SSH_PUB_KEY}}|${_DO_SSH_PUB_KEY_CONTENT}|g" \
-#     -e "s|{{ETCD_DISCOVERY_URL}}|${_ETCD_DISCOVERY}|g" \
-#       ${_CLOUD_CONFIG} \
-#     > ${_CLOUD_CONFIG_GEN}
+sed \
+    -e "s|{{SSH_PORT}}|${SSH_PORT}|g" \
+      ${_CLOUD_CONFIG} \
+    > ${_CLOUD_CONFIG_GEN}
 
 docker-machine create --driver digitalocean \
-    --digitalocean-image coreos-alpha \
+    --digitalocean-image coreos-${COREOS_REL_CHANNEL} \
     --digitalocean-region ${DO_REGION} \
     --digitalocean-size ${DO_SIZE} \
     --digitalocean-ipv6 \
     --digitalocean-private-networking \
     --digitalocean-ssh-user core \
-    --digitalocean-ssh-port 4460 \
+    --digitalocean-ssh-port ${SSH_PORT} \
     ${_ssh_details} \
-    --digitalocean-userdata ${_CLOUD_CONFIG} \
+    --digitalocean-userdata ${_CLOUD_CONFIG_GEN} \
     $DO_NODE_BASENAME
